d6110a9486f160e31dc350df14821f4d1f9e4684
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-19 16:13:35 +0200

    Add ajax request of countries - Add generate random conturies

---
+++ js/country.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+    countryObjects = 0;
+
+    $.ajax({
+        'url' : 'https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json',
+        'method' : 'GET',
+        'success':function (response) {
+             countryObjects = JSON.parse(response);
+
+            changeRandomCountries();
+
+        }
+    });
+
+
+
+    function changeRandomCountries()
+    {
+        var numberOfCountries = Object.keys(countryObjects).length;
+
+        randomCountryArr = [];
+        for (i =0; i< 5; i++){
+            $('#country'+ (i+1) + ' > h4' )
+                    .text(
+                        Object.keys(countryObjects)
+                                [
+                                    Math.floor((Math.random() * numberOfCountries) + 0)
+                                ]
+
+                    );
+        }
+
+
+    }
+});
+
