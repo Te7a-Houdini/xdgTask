d854a8368ece3081cb5d2b7a88f6017c2a58c838
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-20 01:56:24 +0200

    Add hide all except country

--- js/city.js
+++ js/city.js
@@ -7,10 +7,39 @@ $( document ).ready(function() {
             .on('tap', function (event) {
                 var jqueryTabbedElement = $(event.currentTarget);
 
+                if (jqueryTabbedElement.attr('taped') == true)
+                {
 
-            })
+                }
+
+                else
+
+                {
+                    countryTab(jqueryTabbedElement);
+                }
+
+            });
 
 
+
+
+
+    function countryTab(jqueryTabbedElement)
+    {
+
+       $('#wrapper').children().hide('slow');
+
+        $('.circle-container').children().not('#'+jqueryTabbedElement.attr('id')).hide('slow');
+
+        jqueryTabbedElement.attr('taped',true);
+
+        localStorage.setItem("oldCountryLocation", JSON.stringify(jqueryTabbedElement.position()));
+
+        jqueryTabbedElement.offset({top:20,left:90});
+        $('.circle-container').show('slow');
+        jqueryTabbedElement.show('slow');
+
+    }
 
 
 
