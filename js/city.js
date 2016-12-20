f23430d0f55b5f696a1142e29b546ae3623d7e6d
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-20 02:27:30 +0200

    Add draw cities

--- js/city.js
+++ js/city.js
@@ -36,12 +36,50 @@ $( document ).ready(function() {
         localStorage.setItem("oldCountryLocation", JSON.stringify(jqueryTabbedElement.position()));
 
         jqueryTabbedElement.offset({top:20,left:90});
+
         $('.circle-container').show('slow');
+
         jqueryTabbedElement.show('slow');
 
+        drawCities(jqueryTabbedElement)
     }
 
 
+    function drawCities (countryJqueryObj)
+    {
+
+        topMagrin = countryJqueryObj.position().top + 100;
+
+        screenWidth = $(window).width();
+
+        cities = countryObjects[countryJqueryObj.text()];
+
+        tempWidth = 0;
+
+
+        for (i=0; i< cities.length; i ++)
+        {
+            tempWidth += 150;
+
+            if (tempWidth  > screenWidth)
+            {
+                topMagrin +=100;
+                tempWidth = 0;
+            }
+
+            newCity = $('<div  class="cityBalls bubble "><h4 class="cityName">'+cities[i]+'</h4></div>');
+            newCity.offset({top:topMagrin , left:tempWidth});
+
+            $('#wrapper').append(newCity);
+
+            newCity.hide();
+        }
+
+        $('.cityBalls').show('slow');
+
+
+
+    }
 
 });
 
